bcfdc1f6d512c2ac4a0b000d8f30c90ff11d9539
Tresor mt <tresor.tshiabamuteba@yahoo.fr>
2021-08-16 20:50:45 +0300

    share code

---
+++ src/data.js
@@ -0,0 +1,63 @@
+const data={
+    product:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price:90,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name:'adi Slim Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price:150,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name:' leo ',
+            category:'Shirts',
+            image:'/images/p3.jpg',
+            price:180,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name:' Slim ',
+            category:'Shirts',
+            image:'/images/p4.jpg',
+            price:100,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name:'pumt',
+            category:'Shirts',
+            image:'/images/p5.jpg',
+            price:120,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+
+
+    ],
+};
+
+export default data;
